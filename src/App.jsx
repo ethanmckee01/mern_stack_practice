7bd6bf5de6cd4f4641b5ceb26f9fd0a7baacd079
ethanmckee01 <barbwirebill@gmail.com>
2020-03-26 16:03:28 -0400

    Updated server with about API

--- src/App.jsx
+++ src/App.jsx
@@ -58,32 +58,6 @@ function IssueTable(props) {
     );
   }
 
-  render() {
-    const issueRows = this.props.issues.map(issue =>
-      <IssueRow key={issue.id} issue={issue} />
-    );
-
-    return (
-      <table className="bordered-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Status</th>
-            <th>Owner</th>
-            <th>Created</th>
-            <th>Effort</th>
-            <th>Due Date</th>
-            <th>Title</th>
-          </tr>
-        </thead>
-        <tbody>
-          {issueRows}
-        </tbody>
-      </table>
-    );
-  }
-}
-
 class IssueAdd extends React.Component {
   constructor() {
     super();
