815c24dfaae74d82a24b349c115bfba64e789a5e
ethanmckee01 <barbwirebill@gmail.com>
2020-04-13 20:43:10 -0400

    added input validation/error messages

--- server/server.js
+++ server/server.js
@@ -1,24 +1,10 @@
 const fs = require('fs');
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
 
 let aboutMessage = "Issue Tracker API v1.0";
-
-const GraphQLDate = new GraphQLScalarType({
-  name: 'GraphQLDate',
-  description: 'A Date() type in GraphQL as a scalar',
-  serialize(value) {
-    return value.toISOString();
-  },
-  parseValue(value) {
-    return new Date(value);
-  },
-  parseLiteral(ast) {
-    return (ast.kind == Kind.STRING) ? new Date(ast.value) : undefined;
-  },
-});
 
 const issuesDB = [
   {
@@ -33,6 +19,24 @@ const issuesDB = [
   },
 ];
 
+const GraphQLDate = new GraphQLScalarType({
+  name: 'GraphQLDate',
+  description: 'A Date() type in GraphQL as a scalar',
+  serialize(value) {
+    return value.toISOString();
+  },
+  parseValue(value) {
+    const dateValue = new Date(value);
+    return isNaN(dateValue) ? undefined : dateValue;
+  },
+  parseLiteral(ast) {
+    if (ast.kind == Kind.STRING) {
+      const value = new Date(ast.value);
+      return isNaN(value) ? undefined : value;
+    }
+  },
+});
+
 const resolvers = {
   Query: {
     about: () => aboutMessage,
@@ -45,14 +49,6 @@ const resolvers = {
   GraphQLDate,
 };
 
-function issueAdd(_, { issue }) {
-  issue.created = new Date();
-  issue.id = issuesDB.length + 1;
-  if (issue.status == undefined) issue.status = 'New';
-  issuesDB.push(issue);
-  return issue;
-}
-
 function setAboutMessage(_, { message }) {
   return aboutMessage = message;
 }
@@ -61,9 +57,34 @@ function issueList() {
   return issuesDB;
 }
 
+function issueValidate(issue) {
+  const errors = [];
+  if (issue.title.length < 3) {
+    errors.push('Field "title" must be at least 3 characters long.');
+  }
+  if (issue.status === 'Assigned' && !issue.owner) {
+    errors.push('Field "owner" is required when status is "Assigned"');
+  }
+  if (errors.length > 0) {
+    throw new UserInputError('Invalid input(s)', { errors });
+  }
+}
+
+function issueAdd(_, { issue }) {
+  issueValidate(issue);
+  issue.created = new Date();
+  issue.id = issuesDB.length + 1;
+  issuesDB.push(issue);
+  return issue;
+}
+
 const server = new ApolloServer({
   typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
   resolvers,
+  formatError: error => {
+    console.log(error);
+    return error;
+  },
 });
 
 const app = express();
